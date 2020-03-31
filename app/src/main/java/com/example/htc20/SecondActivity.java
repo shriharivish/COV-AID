ffea258f18e8ba067fe6a03ce66533244b18f91f
Harshvardhan Agrawal <agrawalharshvardhan96@gmail.com>
2020-03-31 15:30:39 +0530

    Added QR Code Reader for text, URL, contact info

--- app/src/main/java/com/example/htc20/SecondActivity.java
+++ app/src/main/java/com/example/htc20/SecondActivity.java
@@ -1,26 +1,71 @@
 package com.example.htc20;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.ml.vision.FirebaseVision;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcode;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetector;
+import com.google.firebase.ml.vision.barcode.FirebaseVisionBarcodeDetectorOptions;
+import com.google.firebase.ml.vision.common.FirebaseVisionImage;
+import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata;
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+import com.karumi.dexter.listener.single.PermissionListener;
+import com.otaliastudios.cameraview.CameraView;
+import com.otaliastudios.cameraview.frame.Frame;
+import com.otaliastudios.cameraview.frame.FrameProcessor;
+
+import java.io.PrintWriter;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
     private Button Logout;
     private FirebaseAuth fbase;
-
+    CameraView cameraView;
+    boolean isDetected = false;
+    Button btn_start_again;
+    FirebaseVisionBarcodeDetectorOptions options;
+    FirebaseVisionBarcodeDetector detector;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Logout = (Button) findViewById(R.id.btnLogout);
         fbase = FirebaseAuth.getInstance();
+
+        Dexter.withActivity(this)
+                .withPermissions(new String[]{Manifest.permission.CAMERA, Manifest.permission.RECORD_AUDIO})
+                .withListener(new MultiplePermissionsListener() {
+                    @Override
+                    public void onPermissionsChecked(MultiplePermissionsReport report) {
+                        setupCamera();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(List<PermissionRequest> permissions, PermissionToken token) {
+
+                    }
+                }).check();
 
         Logout.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,4 +79,115 @@ public class SecondActivity extends AppCompatActivity {
 
 
     }
+
+    private void setupCamera() {
+        btn_start_again = (Button) findViewById(R.id.btn_again);
+        btn_start_again.setEnabled(isDetected);
+        btn_start_again.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                isDetected = !isDetected;
+//                btn_start_again.setEnabled(isDetected);
+            }
+        });
+
+        cameraView = (CameraView) findViewById(R.id.cameraView);
+        cameraView.setLifecycleOwner(this);
+        cameraView.addFrameProcessor(new FrameProcessor() {
+            @Override
+            public void process(@NonNull Frame frame) {
+                processImage(getVisionImageFromFrame(frame));
+            }
+        });
+        options = new FirebaseVisionBarcodeDetectorOptions.Builder()
+                .setBarcodeFormats(FirebaseVisionBarcode.FORMAT_QR_CODE)
+                .build();
+
+        detector = FirebaseVision.getInstance().getVisionBarcodeDetector(options);
+
+
+    }
+
+    private FirebaseVisionImage getVisionImageFromFrame(Frame frame) {
+        byte[] data = frame.getData();
+        FirebaseVisionImageMetadata metadata = new FirebaseVisionImageMetadata.Builder()
+                .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_NV21)
+                .setHeight(frame.getSize().getHeight())
+                .setWidth(frame.getSize().getWidth())
+                .build();
+
+        return FirebaseVisionImage.fromByteArray(data, metadata);
+    }
+
+    private void processImage(FirebaseVisionImage image) {
+        if (!isDetected) {
+            detector.detectInImage(image)
+                    .addOnSuccessListener(new OnSuccessListener<List<FirebaseVisionBarcode>>() {
+                        @Override
+                        public void onSuccess(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+                            processResult(firebaseVisionBarcodes);
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(SecondActivity.this, "" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        }
+    }
+
+    private void processResult(List<FirebaseVisionBarcode> firebaseVisionBarcodes) {
+        if (firebaseVisionBarcodes.size() > 0) {
+            isDetected = true;
+            btn_start_again.setEnabled(isDetected);
+            for (FirebaseVisionBarcode item : firebaseVisionBarcodes) {
+                int value_tpe = item.getValueType();
+                switch (value_tpe) {
+                    case FirebaseVisionBarcode.TYPE_TEXT: {
+                        createDialog(item.getRawValue());
+                    }
+                    break;
+
+                    case FirebaseVisionBarcode.TYPE_URL: {
+                        //Start Browser Intent
+                        Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(item.getRawValue()));
+                        startActivity(intent);
+                    }
+                    break;
+
+                    case FirebaseVisionBarcode.TYPE_CONTACT_INFO: {
+                        String info = new StringBuilder("Name: ")
+                                .append(item.getContactInfo().getName().getFormattedName())
+                                .append("\n")
+                                .append("Address")
+                                .append(item.getContactInfo().getAddresses().get(0).getAddressLines())
+                                .append("\nEmail")
+                                .append(item.getContactInfo().getEmails().get(0).getAddress())
+                                .toString();
+                        createDialog(info);
+                    }
+
+                    break;
+
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    private void createDialog(String text) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(text)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
 }
