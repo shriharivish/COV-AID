d113c747d4592cf9e8d777117be7a7755884d632
acharaakshit <acharaakshit@gmail.com>
2020-04-03 17:42:46 +0530

    Updated MapsActivity.java : added button to call google maps and development ongoing for gps access Updated build.gradle added implementation ....

--- app/src/main/java/com/example/htc20/MapsActivity.java
+++ app/src/main/java/com/example/htc20/MapsActivity.java
@@ -1,26 +1,47 @@
 package com.example.htc20;
 
-import androidx.fragment.app.FragmentActivity;
-
+import android.Manifest;
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.FragmentActivity;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-
-import java.net.MalformedURLException;
-import java.net.URL;
+import com.google.android.gms.tasks.OnSuccessListener;
 
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private TextView distance;
+
+    private Button openMaps;
+    private static final int MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION = 1;
+    Context context = this;
     public final static double AVERAGE_RADIUS_OF_EARTH = 6371;
+
+    private LatLng myLoc = new LatLng(-34, 151);
+    private LatLng destLoc = new LatLng(-36,155);
+
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +52,99 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
-        distance = (TextView) findViewById(R.id.tv_distance);
-        float distance_in_kms = calculateDistance(23.0225,72.5714,21.1702,72.8311);
-        distance.setText(String.valueOf(distance_in_kms));
+        //set location
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        String myLoc = fetchLocation();
+
+        //open maps by clicking on the button
+        openMaps = (Button) findViewById(R.id.op_maps);
+        openMaps.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                // click handling code
+                Uri gmmIntentUri = Uri.parse("geo:37.7749,-122.4194");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(mapIntent);
+                }
+            }
+        });
+
+        //distance = (TextView) findViewById(R.id.tv_distance);
+        //float distance_in_kms = calculateDistance(23.0225,72.5714,21.1702,72.8311);
+        //distance.setText(String.valueOf(distance_in_kms));
 
     }
+
+    private String fetchLocation() {
+
+        String loc  = "";
+        if (ContextCompat.checkSelfPermission(MapsActivity.this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(MapsActivity.this,
+                    Manifest.permission.ACCESS_COARSE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                new AlertDialog.Builder(this)
+                        .setTitle("Required Location Permission")
+                        .setMessage("You have to give this permission to acess this feature")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                ActivityCompat.requestPermissions(MapsActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                dialogInterface.dismiss();
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(MapsActivity.this,
+                        new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_COARSE_LOCATION);
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            fusedLocationClient.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                Double latitude = location.getLatitude();
+                                Double longitude = location.getLongitude();
+
+                                String loc = "Latitude = "+latitude + "Longitude = " + longitude;
+                            }
+                        }
+                    });
+
+        }
+       return loc;
+
+    }
+
 
     //function to calculate distance between two coordinates
     private float calculateDistance(double userLat, double userLng, double venueLat, double venueLng) {
@@ -69,9 +178,15 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        //get my location
+
+
+        // Add a marker at both locations and move the camera
+        mMap.addMarker(new MarkerOptions().position(myLoc).title("My Location"));
+        mMap.addMarker(new MarkerOptions().position(destLoc).title("Destination"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(myLoc));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(destLoc));
     }
+
+
 }
