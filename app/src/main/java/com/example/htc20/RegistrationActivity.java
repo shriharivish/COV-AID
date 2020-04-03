3eff625d5a9085cff2156a597f66a267233164c1
Swarup N <nswarup14@gmail.com>
2020-04-03 11:01:01 +0530

    Add user details to the /citizen table in firestore upon new registration (#2)

--- app/src/main/java/com/example/htc20/RegistrationActivity.java
+++ app/src/main/java/com/example/htc20/RegistrationActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,37 +14,83 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class RegistrationActivity extends AppCompatActivity {
 
-    private EditText userName, userPassword, userEmail;
+    private EditText userFirstName, userPassword, userEmail, userLastName, userPhoneNumber;
     private Button register;
     private TextView userLogin;
     private FirebaseAuth firebaseAuth;
+    private FirebaseFirestore db;
+
+    private static final String TAG = "DocSnippets";
+
+    protected void setup(){
+        db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setPersistenceEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-        userName = (EditText) findViewById(R.id.etUserName);
+        userFirstName = (EditText) findViewById(R.id.etFirstName);
+        userLastName = (EditText) findViewById(R.id.etLastName);
+        userPhoneNumber = (EditText) findViewById(R.id.etPhoneNumber);
         userPassword = (EditText) findViewById(R.id.etUserPassword);
         userEmail = (EditText) findViewById(R.id.etUserEmail);
         userLogin = (TextView) findViewById(R.id.tvLogin);
         register = (Button) findViewById(R.id.btnRegister);
 
         firebaseAuth = FirebaseAuth.getInstance();
-
+//        setup();
+        db = FirebaseFirestore.getInstance();
         register.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if (validate()) {
                     String user_email = userEmail.getText().toString().trim();
                     String user_pass = userPassword.getText().toString().trim();
+                    String user_firstName = userFirstName.getText().toString().trim();
+                    String user_lastName = userLastName.getText().toString().trim();
+                    Long user_phoneNumber = Long.parseLong(userPhoneNumber.getText().toString().trim());
+
+                    Map<String, Object> user = new HashMap<>();
+                    user.put("first_name", user_firstName);
+                    user.put("last_name", user_lastName);
+                    user.put("phone_number", user_phoneNumber);
+                    user.put("email", user_email);
+                    db.collection("citizen")
+                            .add(user)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId());
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w(TAG, "Error adding document", e);
+                                }
+                            });
 
                     firebaseAuth.createUserWithEmailAndPassword(user_email, user_pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                         @Override
@@ -71,13 +118,17 @@ public class RegistrationActivity extends AppCompatActivity {
 
     private Boolean validate() {
         Boolean result = false;
-        String name = userName.getText().toString();
+        String firstName = userFirstName.getText().toString();
+        String lastName = userLastName.getText().toString();
         String email = userEmail.getText().toString();
         String password = userPassword.getText().toString();
+        String temp = userPhoneNumber.getText().toString().trim();
+        Long phoneNumber = Long.parseLong(temp);
 
-        if (name.isEmpty() || email.isEmpty() || password.isEmpty()) {
+        if (firstName.isEmpty() || email.isEmpty() || password.isEmpty() || lastName.isEmpty() || phoneNumber == null) {
             Toast.makeText(this, "Please Enter All Details", Toast.LENGTH_SHORT).show();
-        } else {
+        }
+        else {
             result = true;
         }
 
