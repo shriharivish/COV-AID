92ef51143461cc3b3cdc9a9c74a104be0d3bf288
Harshvardhan Agrawal <agrawalharshvardhan96@gmail.com>
2020-04-04 20:04:45 +0530

    Added dynamic list update to hospital list

--- app/src/main/java/com/example/htc20/HospitalListActivity.java
+++ app/src/main/java/com/example/htc20/HospitalListActivity.java
@@ -36,7 +36,6 @@ public class HospitalListActivity extends AppCompatActivity {
     private FusedLocationProviderClient client;
     private int PROXIMITY_RADIUS = 1500;
     private ListView hospital_list;
-    String hospitals[] = {"Hospital1", "Hospital2", "Hospital3", "Hospital4", "Hospital5"};
     private Button mapsAcitivity;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,9 +53,11 @@ public class HospitalListActivity extends AppCompatActivity {
             }
         });
         final ArrayList<String> list = new ArrayList<String>();
-        for (int i = 0; i < hospitals.length; ++i) {
-            list.add(hospitals[i]);
-        }
+
+
+        final ArrayAdapter adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
 
 
         client = LocationServices.getFusedLocationProviderClient(this);
@@ -99,6 +100,7 @@ public class HospitalListActivity extends AppCompatActivity {
                                    Log.d("Longitudes","valueLong : "+HospitalLong);
                                    Log.d("Names","valueName : "+HospitalName);
                                     list.add(HospitalName);
+                                    adapter.notifyDataSetChanged();
                                    //parsing to be done
                                 } catch (JSONException e) {
                                     Log.d("Places", "Error in Adding places");
@@ -120,12 +122,6 @@ public class HospitalListActivity extends AppCompatActivity {
 
             }
         });
-
-        final ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, list);
-        listview.setAdapter(adapter);
-
-
 
 
     }
